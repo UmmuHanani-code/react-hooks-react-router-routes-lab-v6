2019484bf75a18f8df3244bc8f5026a807070e18
Nuria Mohamed Hussein <nuria.hussein@student.moringaschool.com>
2025-03-05 12:32:59 +0300

    commit all

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,23 @@
+
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
-function NavBar() {
-  return (
-    <nav className="navbar">
-    
-    </nav>
-    );
+function NavBar(){
+return(
+  <nav className="navbar">
+    <NavLink to="/" >
+      Home 
+    </NavLink>
+
+    <NavLink to="/directors" >
+      Directors
+    </NavLink>
+
+    <NavLink to="/actors" >
+      Actors
+    </NavLink>
+  </nav>
+);
 };
 
 export default NavBar;
